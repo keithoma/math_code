4a7a381f5e004ba40ce5aa7fc024c97df306a213
Kei <thomakmj@gmail.com>
2020-10-06 15:56:46 +0200

    fixed a bug, the function to compute the coefficients was wrong

--- numerical_analysis/linear_spline_interpolation.py
+++ numerical_analysis/linear_spline_interpolation.py
@@ -15,17 +15,20 @@ class LinearSplineInterpolation:
         self.data_points = data_points
 
         # settings
-        self.local_fineness = 1 # <----- somehow this only works with 1?
+        self.local_fineness = 100
 
     def construct_polynomial(self, start, end):
         """
         start and end should be a list containing exactly two elements
         """
-        s = lambda x : (start[1] * (end[0] - x) - end[1] * (x - start[0])) / (end[0] - start[0])
+        s = lambda x : (start[1] * (end[0] - x) + end[1] * (x - start[0])) / (end[0] - start[0])
         local_grid = np.linspace(start[0], end[0], self.local_fineness)
         return local_grid, [s(x) for x in local_grid]
-    
+
     def construct_spline(self):
+        """
+        pieces together the linear functions constructed above
+        """
         global_grid = []
         spline = []
         for i in range(len(self.data_points) - 1):
@@ -35,17 +38,15 @@ class LinearSplineInterpolation:
         return global_grid, spline
 
 def main():
+    """
+    DEMONSTRATION.
+    """
     x_axis = np.linspace(0, 10, 500)
     sin    = [np.sin(x) for x in x_axis]
-    data_points = [[x, np.sin(x)] for x in np.linspace(0, 9, 10)]
+    data_points = [[x, np.sin(x)] for x in np.linspace(0, 9, 8)]
     obj = LinearSplineInterpolation(data_points)
 
-
     plt.plot(x_axis, sin)
-    print(obj.construct_spline())
-    print(len(obj.construct_spline()[0]))
-    print(len(obj.construct_spline()[1]))
-    # plt.plot([[0, np.sin(0)], [1, np.sin(1)], [2, np.sin(2)], [3, np.sin(3)]])
     plt.plot(*obj.construct_spline())
     plt.show()
 
