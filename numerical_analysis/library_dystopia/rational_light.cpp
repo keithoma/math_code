8efe7bc67550ab1a3c89a03fb7a7f04e6a05a425
Kei Thoma <thomakmj@gmail.com>
2020-10-09 03:46:36 +0200

    late night work

--- numerical_analysis/library_dystopia/rational_light.cpp
+++ numerical_analysis/library_dystopia/rational_light.cpp
@@ -9,44 +9,45 @@ enum class Sign {
     negative
 };
 
-class RationalLight {
-    public:
-        RationalLight(float numerator, float denominator=1.0, Sign sign=Sign::positive);
-
-        unsigned short numerator() { return numerator_; }
-        unsigned short denominator() { return denominator_; }
-        Sign sign() { return sign_; }
-
-        string string_terminal();
-        string string_latex();
-
-    private:
-        // max value for unsigned short is 65535 = 2^16 - 1
-        const unsigned short numerator_;
-        const unsigned short denominator_;
-        const Sign sign_;
-
-        unsigned gcd(unsigned n, unsigned m) { return (m == 0) ? n : gcd(m, n % m); }
-
-        Sign handle_sign(float n, float d, Sign s);
-        pair<unsigned, unsigned> handle_float(float numerator, float denomin);
-        pair<unsigned, unsigned> reduce_fraction(unsigned numerator, unsigned denominator);
+struct Q {
+    Q(unsigned numerator, unsigned denominator, Sign sign)
+        : numerator_{numerator},
+          denominator_{denominator},
+          sign_{sign}
+    {}
+    const unsigned short numerator_;
+    const unsigned short denominator_;
+    const Sign sign_;
 };
 
-Sign RationalLight::handle_sign(float n, float d, Sign s) {
-    return (((n > 0) - (n < 0)) * ((d > 0) - (d < 0)) * ((s == Sign::positive) - (s == Sign::negative)) == 1)
-    ? Sign::positive : Sign::negative;
+// put these things in a class
+// class RationalLight creates the structs Q and handles everything
+Sign handle_sign(float n, float d) {
+    return (((n > 0) - (n < 0)) * ((d > 0) - (d < 0)) == 1) ? Sign::positive : Sign::negative;
 }
 
-pair<unsigned, unsigned> RationalLight::handle_float(float numerator, float denominator) {
-
+pair<unsigned, unsigned> handle_float(float numerator, float denominator) {
+    // do later
+    // for now, we require the arguments to be whole numbers
 }
 
-pair<unsigned, unsigned> RationalLight::reduce_fraction(unsigned numerator, unsigned denominator) {
+unsigned gcd(unsigned n, unsigned m) { return (m == 0) ? n : gcd(m, n % m); }
+
+pair<unsigned, unsigned> reduce_fraction(unsigned numerator, unsigned denominator) {
     const unsigned div = gcd(numerator, denominator);
     return make_pair(numerator / div, denominator / div);
 }
 
+Q rational(float numerator, float denominator=1.0) {
+    const Sign sign = handle_sign(numerator, denominator);
+    const pair<unsigned, unsigned> num_den = reduce_fraction(static_cast<unsigned> (numerator),
+                                                             static_cast<unsigned> (denominator));
+    return Q {num_den.first, num_den.second, sign};
+}
+
 int main() {
+    Q a = rational(15, 3);
+    cout << a.numerator_;
+    cout << a.denominator_;
     return 0;
 }
