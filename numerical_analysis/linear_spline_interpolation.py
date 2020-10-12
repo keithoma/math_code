5fcaaade1d9e1fbc9ab687b6d96db219b55446b9
Kei <thomakmj@gmail.com>
2020-10-12 22:41:52 +0200

    WIP

--- numerical_analysis/linear_spline_interpolation.py
+++ numerical_analysis/linear_spline_interpolation.py
@@ -7,51 +7,83 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+DEFAULT_SETTINGS = {
+    "range": [-1, 1],
+    "fineness": 100, # the fineness of each spline; not to be confused with the number nodes
+    "print-mode": "latex"
+}
+
 class LinearSplineInterpolation:
-    def __init__(self, data_points):
+    def __init__(self, f, n, stg=DEFAULT_SETTINGS):
         """
         data_points should be a list of lists containing exactly two elements
+
+        Arguments:
+            f (callable) : the exact function to be interpolated
+            n (int) : the number of splines, the number of grid points would be (n + 1)
         """
-        self.data_points = data_points
+        self.f = f
+        self.n = n
+        self.stg = stg
 
-        # settings
-        self.local_fineness = 100
+        self.data_points = [[x, f(x)] for x in np.linspace(*stg["range"], n + 1)]
 
-    def construct_polynomial(self, start, end):
+    def data_exact(self):
+        grid = np.linspace(*self.stg["range"], self.stg["fineness"] * self.n)
+        return grid, [self.f(x) for x in grid]
+
+    def data_polynomial(self, start, end):
         """
         start and end should be a list containing exactly two elements
         """
-        s = lambda x : (start[1] * (end[0] - x) + end[1] * (x - start[0])) / (end[0] - start[0])
-        local_grid = np.linspace(start[0], end[0], self.local_fineness)
+        def s(x): return (start[1] * (end[0] - x) + end[1] * (x - start[0])) / (end[0] - start[0])
+        local_grid = np.linspace(start[0], end[0], self.stg["fineness"])
         return local_grid, [s(x) for x in local_grid]
 
-    def construct_spline(self):
+    def data_spline(self):
         """
         pieces together the linear functions constructed above
         """
         global_grid = []
         spline = []
-        for i in range(len(self.data_points) - 1):
-            local_grid, s = self.construct_polynomial(self.data_points[i], self.data_points[i + 1])
+        for i in range(self.n):
+            local_grid, s = self.data_polynomial(self.data_points[i], self.data_points[i + 1])
             global_grid.extend(local_grid)
             spline.extend(s)
         return global_grid, spline
+
+    def string_polynomial(self, start, end):
+        sum1 = "{} * {}".format(start[1], end[0] + " - x")
+        sum2 = "{} * {}".format(end[1], ("x - " + start[0]))
+        return "\\frac{{}}{{}}".format(sum1 + sum2, end[0] - start[0])
+
+"""
+    def string_spline(self):
+        string = ""
+        for i in range(n):
+"""
 
 def main():
     """
     DEMONSTRATION.
     """
     # make this shorter
+    def debug(f, n, stg=DEFAULT_SETTINGS):
+        LSI = LinearSplineInterpolation(f, n, stg)
+        print("n                     = {}".format(LSI.n))
+        print("number of data points = {}".format(len(LSI.data_points)))
+
     def demonstration(f, n, setting={"range": [-1, 1], "fineness": 500}):
-        x_axis = np.linspace(*setting["range"], setting["fineness"])
-        y_axis = [f(x) for x in x_axis]
+        LSI = LinearSplineInterpolation(f, n)
 
-        LSI = LinearSplineInterpolation([[x, f(x)] for x in np.linspace(*setting["range"], n)])
-
-        plt.plot(x_axis, y_axis)
-        plt.plot(*LSI.construct_spline())
+        plt.plot(*LSI.data_spline())
         plt.title("Number of nodes: {}".format(n))
         plt.show()
+
+    debug(lambda x: 1 + 3 * x ** 3 + 5 * x ** 5, 4)
+
+    demonstration(lambda x: 1 + 3 * x ** 3 + 5 * x ** 5, 4)
+
 
     # demonstration with x * sin(x)
     wider_range = {"range": [-10, 10], "fineness": 500}
