43bc4077b0a1cf9f15293e0ebff351a70c1baa6b
Kei <thomakmj@gmail.com>
2021-07-01 04:46:19 +0200

    save

--- numerical_analysis/lagrange_polynomials.py
+++ numerical_analysis/lagrange_polynomials.py
@@ -4,12 +4,30 @@
 1st of July 2021
 """
 
-# from library_dystopia.rationalnumber import RationalNumber as RN
-# from library_dystopia.algebraicnumber import AlgebraicNumber as AN
+from library_dystopia.rationalnumber import RationalNumber as RN
+from library_dystopia.algebraicnumber import AlgebraicNumber as AN
 
-import library_dystopia.rationalnumber as RN
-import library_dystopia.algebraicnumber as AN
+class LagrangePolynomial():
+    def __init__(self, _xdata, _ydata):
+        self.xdata = _xdata
+        self.ydata = _ydata
 
-alg = AN.AlgebraicNumber([RN.RationalNumber(1, 2), RN.RationalNumber(3, 2)], ["pi", "sin(3)"])
+    def lagrange_polynomial(self, j, x):
+        def X(i): return str(self.xdata[i])
+        def Y(i): return str(self.ydata[i])
+        pol = str(self.ydata[j])
+        for i in range(len(self.xdata)):
+            pol += "\\frac{x - {0}}{{1} - {0}} \\cdot".format(X(i), X(j))
+        return self
+
+def main():
+    xdata = [0, 1, 2]
+    ydata = [RN(-1, 2), "sin(pi)", 3]
+    LP = LagrangePolynomial(xdata, ydata)
+    
+
+
+
+alg = AN([RN(1, 2), RN(3, 2)], ["pi", "sin(3)"])
 
 print(alg.string_latex())
--- numerical_analysis/library_dystopia/algebraicnumber.py
+++ numerical_analysis/library_dystopia/algebraicnumber.py
@@ -4,16 +4,27 @@
 1st of July 2021
 """
 
+class ANSetting():
+    def __init__(self):
+        # supported: "terminal", "latex"
+        self.string = "latex"
+
 class AlgebraicNumber():
-    def __init__(self, _coeff, _basis):
+    def __init__(self, _coeff, _basis, _setting=ANSetting()):
         self.coeff = _coeff
         self.basis = _basis
+        self.setting = _setting
     
-    def string_latex(self):
+    def string_latex(self) -> str:
         string = ""
         for i in range(len(self.basis)):
-            string += str(self.coeff[i]) + " " + self.basis[i] + " + "
-        return string
+            string += str(self.coeff[i]) + " \cdot " + self.basis[i] + " + "
+        return string[:-3]
+    
+    def __str__(self) -> str:
+        if self.setting.string == "latex":
+            return self.string_latex()
+        return None
 
 def main():
     coeff = [1, 2, 3]
