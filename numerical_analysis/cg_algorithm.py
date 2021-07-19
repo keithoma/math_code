14461b7930cf9031a6e322ee6934155de888c5ee
Kei <thomakmj@gmail.com>
2021-07-19 18:02:06 +0200

    work from before

---
+++ numerical_analysis/cg_algorithm.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python
+
+"""
+17th of July 2021
+"""
+
+import numpy as np
+
+class CGAlgorithm():
+    def __init__(self, _A, _b, _x0):
+        self.A  = _A
+        self.b  = _b
+        self.x0 = _x0
+
+    def 
+
