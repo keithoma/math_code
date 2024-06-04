c544c63d8b89cc1e611464156bf5afb63113b7f3
Kei <thomakmj@gmail.com>
2024-06-04 19:30:04 +0200

    safe commit

--- formal_power_series/multiplication.py
+++ formal_power_series/multiplication.py
@@ -5,3 +5,23 @@ References:
 https://en.wikipedia.org/wiki/Formal_power_series#Ring_structure
 """
 
+def cauchy_product(a, b, n=10):
+    """
+    """
+    while len(a) < n:
+        a.append(0)
+
+    while len(b) < n:
+        b.append(0)
+
+
+    def c(k): return sum([a[l] * b[k - l] for l in range(k + 1)])
+
+    return [c(k) for k in range(n)]
+
+if __name__ == "__main__":
+    print(cauchy_product([3, 0, 1], [0, -2, 0, 3]))
+
+    import multiplicative_inverse
+
+    print(cauchy_product([1, 6, 9], multiplicative_inverse.multiplicative_inverse([1, 6, 9])))
