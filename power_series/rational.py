1b424a4ed3820001b971d59bff39601ebd7ca6f4
Kei <thomakmj@gmail.com>
2024-06-09 00:41:03 +0200

    well, code is running, but cauchy product is still wrong

--- power_series/rational.py
+++ power_series/rational.py
@@ -5,20 +5,30 @@ import integer
 
 class Rational():
 
-    def __init__(self, numerator, denominator=1):
+    def __init__(self, number, denominator=1):
         """
         """
+        # clearly, this is the most important thing to check
+        if denominator == 0: raise ZeroDivisionError("Denominator cannot be zero.")
+
+        # if the given number is already a rational number and the denominator is 1, we can just
+        # save the values of the given rational number as attributes
+        if isinstance(number, Rational):
+            self.sign        = number.sign
+            self.numerator   = number.numerator
+            self.denominator = number.denominator
+            return
+
         # check if the arguments are integers
         # I don't want to do it the Pythonian way, I need to make sure that we are working with
         # integers
-        if not isinstance(numerator, int): raise TypeError("Numerator must be an integer.")
+        if not isinstance(number, int): raise TypeError("Numerator must be an integer.")
         if not isinstance(denominator, int): raise TypeError("Denominator must be an integer.")
 
-        if denominator == 0: raise ZeroDivisionError("Denominator cannot be zero.")
 
         # save the attributes
-        self.sign        = -1 if numerator * denominator < 0 else 1
-        self.numerator   = int(abs(numerator))
+        self.sign        = -1 if number * denominator < 0 else 1
+        self.numerator   = int(abs(number))
         self.denominator = int(abs(denominator))
 
         # reduce the fraction
@@ -63,9 +73,17 @@ class Rational():
             self.denominator * right.denominator
         )
 
+    def __rmul__(self, right):
+        return Rational(
+            self.sign * self.numerator * right.sign * right.numerator,
+            self.denominator * right.denominator
+        )
+
 if __name__ == "__main__":
     frac1 = Rational(1, 2)
     frac2 = Rational(1, 3)
     frac3 = Rational(1, 4)
-    print(frac1 + frac2 + frac3)
-    print(Rational.rational_sum([frac1, frac2, frac3])) # I have to solve this issue!
+    # print(frac1 + frac2 + frac3)
+    # print(Rational.rational_sum([frac1, frac2, frac3]))
+
+    print(Rational(frac1))
