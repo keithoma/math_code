a4322e65faf5430d9aebc18b53997b962d351738
Kei <thomakmj@gmail.com>
2024-06-11 05:42:01 +0200

    pylint

--- power_series/integer.py
+++ power_series/integer.py
@@ -1,4 +1,8 @@
-"""Colection of small functions.
+"""A colection of small functions.
+
+Currently implemented:
+* gcd(a, b): greatest common divisor of a and b or just a; both a and b can be an int or list
+* lcm(a, b): least common multiple of a and b or just a; both a and b can be an int or list
 """
 
 # allow three or less lettered variables
@@ -7,53 +11,46 @@
 # I think this is a pylint bug with isinstance()
 # pylint: disable=W1116
 
+from typing import Union, List, Optional
+
+IntOrList = Union[int, List[int]]
 
 def _int_gcd(a: int, b: int) -> int:
     """Computes the gcd of two integers."""
-
     while b:
         a, b = b, a % b
     return abs(a)
 
 def _list_gcd(a: list[int]) -> int:
     """Computes the gcd of a list of integers."""
-
     if not a:
         raise ValueError("The list must contain at least one integer.")
-
     result = abs(a[0])
     for number in a[1:]:
         result = _int_gcd(result, abs(number))
-
     return result
 
-def gcd(a: int | list[int], b: int | list[int] | None = None) -> int:
+def gcd(a: IntOrList, b: Optional[IntOrList]=None) -> int:
     """Computes the gcd of ints and lists."""
-
     # this function is supposed to be used with ints in both arguments or a list in the first
     # argument
     if isinstance(a, int) and isinstance(b, int):
         result = _int_gcd(a, b)
-
     elif isinstance(a, list) and b is None:
         result = _list_gcd(a)
 
     # the other combinations are also supported, but will give a warning
     elif isinstance(a, int) and isinstance(b, list):
         result = _list_gcd([a] + b)
-
     elif isinstance(a, int) and b is None:
         result = abs(a)
-
     elif isinstance(a, list) and isinstance(b, int):
         result = _list_gcd(a + [b])
-
     elif isinstance(b, list):
         result = _list_gcd(a + b)
 
     elif not isinstance(a, int | list):
         raise TypeError("Invalid type for first argument. Expected int or list of int.")
-
     elif not isinstance(b, int | list | None):
         print(type(b))
         raise TypeError("Invalid type for second argument. Expected int or list of int.")
@@ -75,40 +72,43 @@ def _list_lcm(a: list[int]) -> int:
         lcm_value = _int_lcm(lcm_value, num)
     return lcm_value
 
-def lcm(a: int | list[int], b: int | list[int] | None=None) -> int:
+def lcm(a: IntOrList, b: Optional[IntOrList]=None) -> int:
     """Computes the lcm of ints and lists."""
-
     # this function is supposed to be used with ints in both arguments or a list in the first
     # argument
     if isinstance(a, int) and isinstance(b, int):
         result = _int_lcm(a, b)
-
     elif isinstance(a, list) and b is None:
         result = _list_lcm(a)
 
     # the other combinations are also supported, but will give a warning
     elif isinstance(a, int) and isinstance(b, list):
         result = _list_lcm([a] + b)
-
     elif isinstance(a, int) and b is None:
         result = abs(a)
-
     elif isinstance(a, list) and isinstance(b, int):
         result = _list_lcm(a + [b])
-
     elif isinstance(b, list):
         result = _list_lcm(a + b)
 
     elif not isinstance(a, int | list):
-        raise TypeError("Invalid type for first argument. Expected int or list of int.")
-
+        raise TypeError("Invalid type for the first argument. Expected int or list of int.")
     elif not isinstance(b, int | list | None):
-        raise TypeError("Invalid type for first argument. Expected int or list of int.")
+        raise TypeError("Invalid type for the second argument. Expected int or list of int.")
 
     return result
 
 def main():
-    pass
+    """Tests the functions implemented."""
+    print()
+    print(f"_int_gcd(24, 300) = {_int_gcd(24, 300)}")
+    print(f"_list_gcd(20, 100, 35) = {_list_gcd([20, 100, 35])}")
+    print(f"gcd(13, 169) = {gcd(13, 169)}")
+    print()
+    print(f"_int_lcm(24, 300) = {_int_lcm(24, 300)}")
+    print(f"_list_lcm(20, 100, 35) = {_list_lcm([20, 100, 35])}")
+    print(f"lcm(13, 169) = {lcm(13, 169)}")
+    print()
 
 if __name__ == "__main__":
     main()
--- power_series/integer.tests.py
+++ power_series/integer.tests.py
@@ -1,71 +1,53 @@
+""" Tests the functions implemented in integer.py.
+"""
 
 # set pylint to allow variable names under three letters
 # pylint: disable=C0103
 
-
+from typing import Union, List, Optional
 import numpy as np
+import integer
 
-def _nth_test(n: int, a: int | list[int], b: int | list[int] | None=None) -> int:
-    """ Executes a single test of gcd() and lcm(). For internal use only.
+IntOrList = Union[int, List[int]]
 
-    Args:
-        n (int): the current count of tests
-        a (int): an integer
-        b (int): an integer
-
-    Returns:
-        (int): n from the argument incremented by 1
-    """
-    computed_gcd = gcd(a, b)
-    computed_lcm = lcm(a, b)
-
+def _make_list(a: IntOrList, b: Optional[IntOrList]=None) -> list:
+    """Combines the values of a and b to a single list."""
     if isinstance(a, int) and isinstance(b, int):
-        computed_aux_gcd = _int_gcd(a, b)
-        computed_aux_lcm = _int_lcm(a, b)
+        result = [a, b]
+    elif isinstance(a, int) and isinstance(b, list):
+        result = [a] + b
+    elif isinstance(a, int) and b is None:
+        result = [a]
+    elif isinstance(a, list) and isinstance(b, int):
+        result = a + [b]
+    elif isinstance(b, list):
+        result = a + b
+    elif isinstance(a, list) and b is None:
+        result = a
+    return result
 
-        true_gcd = np.gcd(a, b)
-        true_lcm = np.lcm(a, b)
-
-    elif isinstance(a, list) and not None:
-        computed_aux_gcd = _list_gcd(a)
-        computed_aux_lcm = _list_lcm(a)
-
-        true_gcd = np.gcd.reduce(a)
-        true_lcm = np.lcm.reduce(a)
-
-    else:
-        computed_aux_gcd = "NA "
-        computed_aux_lcm = "NA "
-        true_gcd = "NA "
-        true_lcm = "NA "
+def _nth_test(n: int, a: IntOrList, b: Optional[IntOrList]=None)  -> int:
+    """Executes a single test of gcd() and lcm()."""
+    computed_gcd = integer.gcd(a, b)
+    computed_lcm = integer.lcm(a, b)
+    true_gcd = np.gcd.reduce(_make_list(a, b))
+    true_lcm = np.lcm.reduce(_make_list(a, b))
 
     print("--------------------------------------------------")
     print("Test {n}:\n"
-          "gcd({a}, {b}) = {computed_gcd}. _int_gcd({a}, {b}) = {computed_aux_gcd}."
-          "Should be {true_gcd}.\n"
-          "lcm({a}, {b}) = {computed_lcm}. _int_lcm({a}, {b}) = {computed_aux_lcm}."
-          "Should be {true_lcm}".format(
+          "gcd({a}, {b}) = {computed_gcd}. Should be {true_gcd}.\n"
+          "lcm({a}, {b}) = {computed_lcm}. Should be {true_lcm}".format(
               n=n, a=a, b=b,
-              computed_gcd=computed_gcd, computed_aux_gcd=computed_aux_gcd, true_gcd=true_gcd,
-              computed_lcm=computed_lcm, computed_aux_lcm=computed_aux_lcm, true_lcm=true_lcm))
+              computed_gcd=computed_gcd, true_gcd=true_gcd,
+              computed_lcm=computed_lcm, true_lcm=true_lcm))
 
-    if true_gcd == "NA " or true_lcm == "NA ":
-        pass
-    elif computed_gcd != true_gcd or computed_lcm != true_lcm:
-        raise Exception("Something went wrong. Computed value doesn't match the actual value.")
+    if computed_gcd != true_gcd or computed_lcm != true_lcm:
+        raise ValueError("Something went wrong. Computed value doesn't match the actual value.")
 
     return n + 1
 
 def _test(number_of_random_tests: int=100, interval: tuple[int, int]=(-8, 9)) -> None:
-    """ Executes a batch of tests. For internal use only.
-
-    Args:
-        number_of_random_tests (int): the number of random tests executed in a batch
-        interval (tuple of int): two ints as a tuple, left value is inclusive, right value is
-            exclusive
-
-    Returns
-    """
+    """Executes a batch of tests. For internal use only."""
     # counter for n-th test
     n = 1
 
@@ -121,9 +103,12 @@ def _test(number_of_random_tests: int=100, interval: tuple[int, int]=(-8, 9)) ->
 
         n = _nth_test(n, a, b)
 
+    # pylint: disable=useless-return
     return None
 
+def main(number_of_random_tests=100, interval=(-8, 9)):
+    """Executes the tests."""
+    _test(number_of_random_tests, interval)
+
 if __name__ == "__main__":
-    NUMBER_OF_RANDOM_TESTS = 100
-    INTERVAL = (-8, 9)
-    _test(NUMBER_OF_RANDOM_TESTS, INTERVAL)
+    main()
