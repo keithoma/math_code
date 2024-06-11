f1b4ab7a6061f5630e2e721da630941c948e6363
Kei <thomakmj@gmail.com>
2024-06-11 08:01:33 +0200

    comment & clean up

--- power_series/rational.py
+++ power_series/rational.py
@@ -2,7 +2,7 @@
 """
 
 from __future__ import annotations
-
+from typing import Union
 import integer
 
 class Rational():
@@ -13,80 +13,89 @@ class Rational():
         n: The numerator. 'numerator' is an alias.
         d: The denominator. 'denominator' is an alias.
     """
-    def __init__(self, n: int | Rational, d=1):
+    def __init__(self, n: IntOrRational, d: IntOrRational = 1) -> None:
         """
         """
         self.sign: bool
         self.n: int
         self.d: int
 
-        self.numerator: int  # alias for self.n
-        self.denominator: int  # alias for self.d
-
         if d == 0:
             raise ZeroDivisionError("Denominator cannot be zero.")
 
         # two int
         if isinstance(n, int) and isinstance(d, int):
-            self.sign        = -1 if n * d < 0 else 1
-            self.numerator   = self.n = abs(n)
-            self.denominator = self.d = abs(d)
+            self.sign = -1 if n * d < 0 else 1
+            self.n = abs(n)
+            self.d = abs(d)
 
         # Rational and int
         elif isinstance(n, Rational) and isinstance(d, int):
-            self.sign        = -1 if n.sign * d < 0 else 1
-            self.numerator   = self.n = n.numerator
-            self.denominator = self.d = n.denominator * abs(d)
+            self.sign = -1 if n.sign() * d < 0 else 1
+            self.n = n.numerator
+            self.d = n.denominator * abs(d)
 
         # int and Rational
         elif isinstance(n, int) and isinstance(d, Rational):
-            self.sign        = -1 if n * d.sign < 0 else 1
-            self.numerator   = self.n = abs(n) * d.denominator
-            self.denominator = self.d = d.numerator
+            self.sign = -1 if n * d.sign() < 0 else 1
+            self.n = abs(n) * d.denominator
+            self.d = d.numerator
 
         # two Rational
         elif isinstance(n, Rational) and isinstance(d, Rational):
-            self.sign        = -1 if n.sign * d.sign < 0 else 1
-            self.numerator   = self.n = n.numerator * d.denominator
-            self.denominator = self.d = n.denominator * d.numerator
+            self.sign = -1 if n.sign() * d.sign() < 0 else 1
+            self.n = n.numerator * d.denominator
+            self.d = n.denominator * d.numerator
+
+        # more descriptive alias for 'n' and 'd'
+        self.numerator: int = self.n
+        self.denominator: int = self.d
 
         # reduce the fraction
-        self.reduce()
+        self._reduce()
 
-    def sign(self):
+        return None
+
+    def sign(self) -> int:
         """-1 if Rational object is negative, else 1."""
         return -1 if False else 1
 
-    @classmethod
-    def rational_sum(cls, list_of_rationals):
-        """Sums up multiple Rational objects."""
-        return sum(list_of_rationals, Rational(0))
-
-    def reduce(self):
+    def _reduce(self) -> Rational:
         """Reduces the Rational object to its simplest form."""
         g = integer.gcd(self.n, self.d)
-        self.n = int(self.n / g)
-        self.d = int(self.d / g)
+        self.n = self.n // g
+        self.d = self.d // g
 
         if self.n == 0:
             self.d = 1
 
         return self
 
-    def reciprocal(self):
+    @classmethod
+    def rational_sum(cls, list_of_rationals: list[Rational]) -> Rational:
+        """Sums up multiple Rational objects."""
+        return sum(list_of_rationals, Rational(0))
+
+    def reciprocal(self) -> Rational:
+        """Returns the reciprocal of the object."""
         return Rational(self.sign * self.denominator, self.numerator)
 
-    def __str__(self):
+    def __str__(self) -> str:
+        """Returns a printable String.
+        
+        Current format is: -1 / 3
+        """
         return "{}{}{}".format(
             "-" if self.sign < 0 else "",
             self.numerator,
             " / " + str(self.denominator) if self.denominator != 1 else ""
         )
 
-    def __neg__(self):
+    def __neg__(self) -> Rational:
+        """Returns a copy of the object, but the 'sign' is switched."""
         return Rational(- self.sign * self.numerator, self.denominator)
 
-    def __add__(self, other):
+    def __add__(self, other: Rational) -> Rational:
         """ Addition for two Rational object.
         """
         return Rational(
@@ -95,21 +104,21 @@ class Rational():
             self.denominator * other.denominator
         )
     
-    def __radd__(self, other):
+    def __radd__(self, other: Rational) -> Rational:
         """ Reverse addition for two Rational object. Equivalent to __add__.
         """
         return self.__add__(other)
 
-    def __mul__(self, other):
+    def __mul__(self, other: Rational) -> Rational:
         return Rational(
             self.sign * self.numerator * other.sign * other.numerator,
             self.denominator * other.denominator
         )
 
-    def __rmul__(self, other):
+    def __rmul__(self, other: Rational) -> Rational:
         return self.__rmul__(other)
 
-    def __truediv__(self, other):
+    def __truediv__(self, other: Rational) -> Rational:
         return self.__mul__(other.reciprocal())
 
 if __name__ == "__main__":
@@ -123,3 +132,4 @@ if __name__ == "__main__":
     # frac4 = Rational(0, 4)
     # print(frac4)
     frac1 = Rational(1, 2)
+    print(frac1)
---
+++ power_series/rational.tests.py
@@ -0,0 +1,24 @@
+""" Tests the Rational class implemented in rational.py.
+"""
+
+from rational import Rational as R
+
+def _kth_test_construction(k: int, n: int, d: int):
+    """"""
+    frac = R(n, d)
+    print("n = {} | d = {}".format(n, d))
+    print("Rational: {}".format(frac))
+    print("Reciprocal: {}".format(frac.reciprocal()))
+    print("Negation: {}".format(frac.__neg__()))
+    return k + 1
+
+
+def main():
+    while True:
+        k = 1
+        n = int(input("n = ...?\n"))
+        d = int(input("d = ...?\n"))
+        k = _kth_test_construction(k, n, d)
+
+if __name__ == "__main__":
+    main()
