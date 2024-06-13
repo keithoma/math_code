46bc587d3c60959e4433ac3ee1f032cb11b37607
Kei <thomakmj@gmail.com>
2024-06-13 23:30:41 +0200

    clean up

--- power_series/rational.py
+++ power_series/rational.py
@@ -13,42 +13,38 @@ class Rational():
         d: The denominator. 'denominator' is an alias.
     """
     # pylint: disable-next=useless-return
-    def __init__(self, n: int | Rational, d: int | Rational = 1) -> None:
+    def __init__(self, top: int | Rational, bottom: int | Rational = 1) -> None:
         """Constructs a Rational object."""
-        self.sign: int
-        self.n: int
-        self.d: int
+        self._sign: int
+        self._numerator: int
+        self._denominator: int
 
-        if d == 0:
+        if bottom == 0:
             raise ZeroDivisionError("Denominator cannot be zero.")
 
         # two int
-        if isinstance(n, int) and isinstance(d, int):
-            self.sign = -1 if n * d < 0 else 1
-            self.n = abs(n)
-            self.d = abs(d)
+        if isinstance(top, int) and isinstance(bottom, int):
+            self._sign = -1 if top * bottom < 0 else 1
+            self._numerator = abs(top)
+            self._denominator = abs(bottom)
 
         # Rational and int
-        elif isinstance(n, Rational) and isinstance(d, int):
-            self.sign = -1 if n.sign * d < 0 else 1
-            self.n = n.numerator
-            self.d = n.denominator * abs(d)
+        elif isinstance(top, Rational) and isinstance(bottom, int):
+            self._sign = -1 if top.sign * bottom < 0 else 1
+            self._numerator = top.n
+            self._denominator = top.d * abs(bottom)
 
         # int and Rational
-        elif isinstance(n, int) and isinstance(d, Rational):
-            self.sign = -1 if n * d.sign < 0 else 1
-            self.n = abs(n) * d.denominator
-            self.d = d.numerator
+        elif isinstance(top, int) and isinstance(bottom, Rational):
+            self._sign = -1 if top * bottom.sign < 0 else 1
+            self._numerator = abs(top) * bottom.d
+            self._denominator = bottom.n
 
         # two Rational
-        elif isinstance(n, Rational) and isinstance(d, Rational):
-            self.sign = -1 if n.sign * d.sign < 0 else 1
-            self.n = n.numerator * d.denominator
-            self.d = n.denominator * d.numerator
-
-        # more descriptive alias for 'n' and 'd'
-        self.numerator: int = self.n
-        self.denominator: int = self.d
+        elif isinstance(top, Rational) and isinstance(bottom, Rational):
+            self._sign = -1 if top.sign * bottom.sign < 0 else 1
+            self._numerator = top.n * bottom.d
+            self._denominator = top.d * bottom.n
 
         # reduce the fraction
         self._reduce()
@@ -57,15 +53,45 @@ class Rational():
 
     def _reduce(self) -> None:
         """Reduces the Rational object to its simplest form."""
-        if self.n == 0:
-            self.d = 1
+        if self._numerator == 0:
+            self._denominator = 1
             return None
 
-        g = integer.gcd(self.n, self.d)
-        self.n = self.n // g
-        self.d = self.d // g
+        g = integer.gcd(self._numerator, self._denominator)
+        self._numerator = self._numerator // g
+        self._denominator = self._denominator // g
 
         return None
+
+    @property
+    def sign(self):
+        """Getter for the attribute '_sign'."""
+        return self._sign
+
+    @property
+    def numerator(self):
+        """Getter for the attribute '_numerator'."""
+        return self._numerator
+
+    @property
+    def signed_numerator(self):
+        """Returns the numerator with the appropriate sign."""
+        return self._sign * self._numerator
+
+    @property
+    def denominator(self):
+        """Getter for the attribute '_denominator'."""
+        return self._denominator
+
+    @property
+    def signed_denominator(self):
+        """Returns the denominator with the appropriate sign."""
+        return self._sign * self._denominator
+
+    n = numerator
+    signed_n = signed_numerator
+    d = denominator
+    signed_d = signed_denominator
 
     @classmethod
     def rational_sum(cls, list_of_rationals: list[Rational]) -> Rational:
@@ -74,7 +100,7 @@ class Rational():
 
     def reciprocal(self) -> Rational:
         """Returns the reciprocal of the object."""
-        return Rational(self.sign * self.denominator, self.numerator)
+        return Rational(self.signed_denominator, self.numerator)
 
     def __str__(self) -> str:
         """Returns a printable String.
@@ -82,7 +108,7 @@ class Rational():
         Current format is: -1 / 3
         """
         pm = "-" if self.sign < 0 else ""
-        right = " / " + str(self.denominator) if self.denominator != 1 else ""
+        right = " / " + str(self.d) if self.d != 1 else ""
         return f"{pm}{self.n}{right}"
 
     def __repr__(self) -> str:
@@ -147,13 +173,33 @@ class Rational():
 
 def main():
     """Tests the class and methods implemented in this file."""
+    # we will often use this shorthand
     R = Rational
-    frac1 = R(1, 2)
-    frac2 = R(-2, 5)
-    frac3 = R(frac1, 3)
-    frac4 = R(-7, frac2)
-    frac5 = R(frac2, frac1)
 
+    # here, we test the constructor
+    frac1 = R(10, 20)
+    print(f"R(10, 20) = {frac1}. (Should be '1 / 2'.)")
+
+    frac2 = R(-6, 15)
+    print(f"R(-6, 15) = {frac2}. (Should be '-2 / 5'.)")
+
+    frac3 = R(frac1, 3)
+    print(f"R((1/2) / 3) = {frac3}. (Should be '1 / 6'.)")
+
+    frac4 = R(-7, frac2)
+    print(f"R(-7 / (-2/5)) = {frac4}. (Should be '35 / 2'.)")
+
+    frac5 = R(frac2, frac1)
+    print(f"R((-2/5) / (1/2)) = {frac5}. (Should be '-4 / 5'.)")
+
+    # test the property functions
+    print(f"R(-2, 5).sign = {frac2.sign}")
+    print(f"R(-2, 5).numerator = {frac2.numerator}")
+    print(f"R(-2, 5).n = {frac2.n}")
+    print(f"R(-2, 5).signed_numerator = {frac2.signed_numerator}")
+    print(f"R(-2, 5).signed_n = {frac2.signed_n}")
+    print(f"R(-2, 5).denominator = {frac2.denominator}")
+    print(f"R(-2, 5).d = {frac2.d}")
 
 if __name__ == "__main__":
     main()
