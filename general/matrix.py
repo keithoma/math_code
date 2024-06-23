93a86af5bba61a3da03135405d035f01361d67a2
Kei Thoma <thomakmj@gmail.com>
2024-06-23 16:31:14 +0200

    clean up

--- general/matrix.py
+++ general/matrix.py
@@ -9,8 +9,13 @@ Number = int | R
 
 class Matrix():
     """A (rational valued) m x n matrix.
+    
+    Attributes:
+        _data (list[list[Number]]): The entries of the matrix as a list of
+            list of Rational.
     """
     def __init__(self, raw: list[list[Number]]):
+        """Constructs a matrix object."""
         # check 'raw' for type
         if not all(row and isinstance(row, list) for row in raw):
             raise TypeError("Input is not a list of list of Number.")
@@ -22,20 +27,20 @@ class Matrix():
         if len(set(len(row) for row in self._data)) > 1:
             raise ValueError("All rows must have the same number of columns.")
 
-        # save the shape
-        # TODO:
-        self._shape = None
-
     ## Properties
     @property
-    def row(i: int) -> list[Number]:
-        # TODO:
-        pass
+    def shape(self) -> tuple[int]:
+        """The shape of the matrix as a tuple of two ints. First value, m, is
+        the number of rows, while the second value, n, is the number of
+        columns.
+        """
+        return len(self._data), len(self._data[0])
 
     @property
-    def column(j: int) -> list[Number]:
-        # TODO:
-        pass
+    def is_square(self) -> bool:
+        """True if the matrix square, false otherwiese."""
+        m, n = self.shape
+        return m == n
 
     ## Magic Methods
     ### Comparisons
@@ -74,12 +79,10 @@ class Matrix():
     def __mul__(self, other: Matrix) -> Matrix:
         """Multiplication for two Rational objects."""
         # TODO: implement this
-        pass
 
     def __pow__(self, other: int) -> Matrix:
         """Exponentiation of a Rational number to an integer power."""
         # TODO: implement this
-        pass
 
     ### Reflected Arithmetic Operators
     def __radd__(self, other: Matrix) -> Matrix:
@@ -100,33 +103,53 @@ class Matrix():
         """Returns a printable String.
         """
         # TODO: implement this
-        pass
+        return str(self._data)  # this is just a placeholder
 
     def __repr__(self) -> str:
         """Returns a string representation of the Rational object."""
         # TODO: implement this
-        pass
+        return str(self._data)  # this is just a placeholder
 
     ## Instance Methods
-    def scalar_multiplication(self, scalar: Number) -> Matrix:
+    def row(self, i: int) -> list[Number]:
+        """Returns the i-th row."""
         # TODO:
-        pass
+
+    def column(self, j: int) -> list[Number]:
+        """Returns the j-th column."""
+        # TODO:
+
+    def cell(self, i: int, j:int) -> Number:
+        """Returns the entry at i-th row and j-th column."""
+        return self._data[i - 1][j - 1]
+
+    def scalar_multiplication(self, scalar: Number) -> Matrix:
+        """Returns the product of the matrix and the scalar."""
+        # TODO:
+
+    def transpose(self) -> Matrix:
+        """Returns the transpose."""
+        # TODO:
 
 def main() -> None:
+    """Tests the class and methods implemented in this file."""
     # check for typing
     faulty_raws = [
-        "a",              # not even a list
-        [1, 2],           # not a list of list
-        [[]],             # list of list, but empty
-        [["a"]],          # list of list, but contains not a Number
-        [[1, R(2), "a"]]  # same as above
+        "a",               # not even a list
+        [1, 2],            # not a list of list
+        [[]],              # list of list, but empty
+        [["a"]],           # list of list, but contains not a Number
+        [[1, R(2), "a"]],  # same as above
+        [[1, 2],
+         [1]]              # mismatched rows
     ]
+
 
     for m in faulty_raws:
         try:
             Matrix(m)
             print(f"Incorrectly accepted \nm = {m}.")
-        except TypeError as error:
+        except (TypeError, ValueError) as error:
             print(f"Correctly rejected \nm = {m}.\nError: {error}")
             print()
 
@@ -135,19 +158,28 @@ def main() -> None:
         [[1, R(2)],
          [R(0), R(R(3, 5))]]
     )
-    print(m1._data)
+    print(m1)
 
     m2 = Matrix(
         [[1, 0],
          [0, 0]]
     )
-    print(m2._data)
+    print(m2)
 
     m3 = m1 + m2
     m4 = m1 - m2
 
-    print(m3._data)
-    print(m4._data)
+    print(m3)
+    print(m4)
+    print("\n" + "-" * 80 + "\n\n")
+
+    # check for cell
+    print(m1)
+    print(f"Shape: {m1.shape} | Is Square? = {m1.is_square}")
+    print(m1.cell(1, 1))
+    print(m1.cell(1, 2))
+    print(m1.cell(2, 1))
+    print(m1.cell(2, 2))
 
 if __name__ == "__main__":
     main()
