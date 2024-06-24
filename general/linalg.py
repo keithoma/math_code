936e855ee92a859ed57979d60a615aebfe1ff64d
Kei Thoma <thomakmj@gmail.com>
2024-06-24 17:37:16 +0200

    I think we will need some more classes

---
+++ general/linalg.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+"""TODO: write this"""
+
+from rational import Rational as R
+from matrix import Matrix as M
+
+class LinAlg():
+    def __init__(self):
+        """TODO: write this"""
+
+    def matrix_multiplication_with_steps(self, a: M, b: M) -> str:
+        """TODO: write this
+        
+        maybe we want to have a steps object ...
+        """
